4f4dc7d24934b42f38a955cd3ad43fed8281356a
jake chadwell <jake.chadwell01@gmail.com>
2021-09-21 00:32:36 -0500

    started generating password function

--- src/script.js
+++ src/script.js
@@ -1,20 +1,40 @@
-const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
-const numbers = "0123456789";
-const symbols = "!@#$%^&*()_+=";
+ const upper = document.getElementById("upper");
+ const lower = document.getElementById("lower");
+ const symbol = document.getElementById("symbol");
+ const num = document.getElementById("num");
+ const len = document.getElementById("len");
+ const pw = document.getElementById("pw");
+ const copy = document.getElementById("copy");
+ const generate = document.getElementById("generate");
 
-function getLowercase() {
-    return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
-}
+ const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+ const numbers = "0123456789";
+ const symbols = "!@#$%^&*()_+=";
 
-function getUppercase() {
-    return upperLetters[Math.floor(Math.random() * upperLetters.length)];
-}
+ function getLowercase() {
+     return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+ }
 
-function getNumber() {
-    return numbers[Math.floor(Math.random() * numbers.length)];
-}
+ function getUppercase() {
+     return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+ }
 
-function getSymbol() {
-    return symbols[Math.floor(Math.random() * symbols.length)];
-}
+ function getNumber() {
+     return numbers[Math.floor(Math.random() * numbers.length)];
+ }z
+
+ function getSymbol() {
+     return symbols[Math.floor(Math.random() * symbols.length)];
+ }
+
+ function generatePassword() {
+    let length = len.value;
+    let password = "";
+
+    for(let i = ; i<){
+        j+=1;
+    }
+    let random = Math.floor(Math.random() * j);
+
+ }
